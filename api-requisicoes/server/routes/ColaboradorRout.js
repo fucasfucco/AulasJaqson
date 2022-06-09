178d5b091c96848edfe6668c3cfee2214f8dab9e
LUCAS GOIS DA SILVA <9001929@riachuelo.com.br>
2022-06-09 11:12:29 -0300

    Mudança de lógica de token do arquivo index.js para o arquivo colaboradorRout.js

--- api-requisicoes/server/routes/ColaboradorRout.js
+++ api-requisicoes/server/routes/ColaboradorRout.js
@@ -2,6 +2,26 @@ const express = require("express");
 const routes = express.Router();
 const controle = require("../controller/ColaboradorCont");
 
+const jwt = require("jsonwebtoken");
+routes.use(function (req, res, next) { // interceptar as requisições a validar o token
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.status(403).send(
+            { message: 'Não possui token de autenticação. Acesso não autorizado!' });
+        jwt.verify(token, process.env.JWT_PRIV_KEY, function (err, decoded) {
+            if (err) return res.status(500).send({
+                auth: false,
+                message: 'Token inválido. Acesso não autorizado!'
+            });
+            // estando tudo certo guarda no request para uso posterior
+            req.userId = decoded._id;
+            req.userName = decoded.nome;
+            next();
+        });
+    } catch (error) { res.status(400).send("Erro no token de autenticação!"); }
+});
+
+
 routes.route("/colaboradores").get(controle.listar);
 routes.route("/colaboradores").post(controle.incluir);
 routes.route("/colaboradores").put(controle.alterar);
